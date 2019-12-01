fad6520e2f9199d277eca87e2193d0cc44428340
Lypheo <saifullah124@hotmail.de>
2019-12-01 09:53:10 +0100

    refactor day1.py

--- 2019/day1.py
+++ 2019/day1.py
@@ -1,26 +1,14 @@
-from datetime import datetime
-from aocd.models import Puzzle
-from aocd import submit
+input_data = open("bigboyinput", "r").read()[:-1]
 
-day = datetime.today().day
-puzzle = Puzzle(year=2019, day=day)
-input_data = puzzle.input_data
-
-def solve_a(inp=input_data):
-    return sum([int(i) // 3 - 2 for i in inp.split("\n")])
-
-def solve_b(inp=input_data):
+def solve(inp=input_data):
     def get_fuel(mass):
         fuel = mass // 3 - 2
         return 0 if fuel < 1 else fuel + get_fuel(fuel)
 
     masses = [int(i) // 3 - 2 for i in inp.split("\n")]
-    return sum([get_fuel(mass) + mass for mass in masses])
+    part1 = sum(masses)
+    return part1, sum(get_fuel(mass) for mass in masses) + part1
 
-a = solve_a()
+a, b = solve()
 print(f"Part 1: {a}")
-# submit(a, part="a", day=day, year=2019)
-
-b = solve_b()
-if b: print(f"Part 2: {b}")
-# submit(b, part="b", day=day, year=2019)
+print(f"Part 2: {b}")
