0094840e69deee026d734cab70b303410d759a33
Lypheo <saifullah124@hotmail.de>
2019-12-01 06:37:19 +0100

    Day 1

--- 2019/day1.py
+++ 2019/day1.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 import time
-import functools, itertools, collections, re
+import functools, itertools, collections, re, math
 import numpy as np
 from aocd.models import Puzzle
 from aocd import submit
@@ -8,17 +8,36 @@ from aocd import submit
 day = datetime.today().day
 puzzle = Puzzle(year=2019, day=day)
 input_data = puzzle.input_data
+# print(input_data)
 
 def solve_a(inp=input_data):
-    return None
+    masses = [int(i) for i in inp.split("\n")]
+    total = 0
+    for i in masses:
+        total += math.floor(i/3) -2
+    return total
 
 def solve_b(inp=input_data):
-    return None
+    masses = [int(i) for i in inp.split("\n")]
+    total = 0
+    for i in masses:
+        fuel = (i//3) - 2
+        diff = fuel
+        while True:
+            diff = ((diff)//3) - 2
+            if diff > 0:
+                fuel += diff
+            else: 
+                break
+                
+        total += fuel
+
+    return total
 
 a = solve_a()
-print("Part 1: {a}")
+print(f"Part 1: {a}")
 # submit(a, part="a", day=day, year=2019)
 
 b = solve_b()
-if b: print("Part 2: {b}")
+if b: print(f"Part 2: {b}")
 # submit(b, part="b", day=day, year=2019)
--- template.py
+++ template.py
@@ -16,9 +16,9 @@ def solve_b(inp=input_data):
     return None
 
 a = solve_a()
-print("Part 1: {a}")
+print(f"Part 1: {a}")
 # submit(a, part="a", day=day, year=2019)
 
 b = solve_b()
-if b: print("Part 2: {b}")
+if b: print(f"Part 2: {b}")
 # submit(b, part="b", day=day, year=2019)
