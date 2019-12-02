2d451ee902cc276d130550960101279b67821b53
Lypheo <saifullah124@hotmail.de>
2019-12-02 07:12:01 +0100

    Add day2

---
+++ 2019/day2.py
@@ -0,0 +1,26 @@
+day = datetime.today().day
+puzzle = Puzzle(year=2019, day=day)
+input_data = puzzle.input_data
+
+#
+def solve(inp=input_data, noun=12, verb=2):
+    inp = [int(i) for i in inp.split(",")]
+    inp[1:3] = [noun, verb]
+    for i in range(0, len(inp), 4):
+        opcode = inp[i]
+        if opcode == 1:
+            inp[inp[i+3]] = inp[inp[i+1]] + inp[inp[i+2]]
+        elif opcode == 2:
+            inp[inp[i+3]] = inp[inp[i+1]] * inp[inp[i+2]]
+        elif opcode == 99:
+            break
+
+    return inp[0]
+
+print(f"Part 1: {solve()}")
+
+for noun in range(100):
+    for verb in range(100):
+        if solve(noun=noun, verb=verb) == 19690720:
+            print(f"Part 2: {100 * noun + verb}")
+            break
