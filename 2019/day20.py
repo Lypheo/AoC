50616820d1e2af51266ed3a06207be0970bc1171
Lypheo <saifullah124@hotmail.de>
2019-12-20 16:10:48 +0100

    day 20 gold

--- 2019/day20.py
+++ 2019/day20.py
@@ -10,106 +10,6 @@ day = datetime.today().day
 puzzle = Puzzle(year=2019, day=day)
 input_data = puzzle.input_data
 
-test1 = """             Z L X W       C                 
-             Z P Q B       K                 
-  ###########.#.#.#.#######.###############  
-  #...#.......#.#.......#.#.......#.#.#...#  
-  ###.#.#.#.#.#.#.#.###.#.#.#######.#.#.###  
-  #.#...#.#.#...#.#.#...#...#...#.#.......#  
-  #.###.#######.###.###.#.###.###.#.#######  
-  #...#.......#.#...#...#.............#...#  
-  #.#########.#######.#.#######.#######.###  
-  #...#.#    F       R I       Z    #.#.#.#  
-  #.###.#    D       E C       H    #.#.#.#  
-  #.#...#                           #...#.#  
-  #.###.#                           #.###.#  
-  #.#....OA                       WB..#.#..ZH
-  #.###.#                           #.#.#.#  
-CJ......#                           #.....#  
-  #######                           #######  
-  #.#....CK                         #......IC
-  #.###.#                           #.###.#  
-  #.....#                           #...#.#  
-  ###.###                           #.#.#.#  
-XF....#.#                         RF..#.#.#  
-  #####.#                           #######  
-  #......CJ                       NM..#...#  
-  ###.#.#                           #.###.#  
-RE....#.#                           #......RF
-  ###.###        X   X       L      #.#.#.#  
-  #.....#        F   Q       P      #.#.#.#  
-  ###.###########.###.#######.#########.###  
-  #.....#...#.....#.......#...#.....#.#...#  
-  #####.#.###.#######.#######.###.###.#.#.#  
-  #.......#.......#.#.#.#.#...#...#...#.#.#  
-  #####.###.#####.#.#.#.#.###.###.#.###.###  
-  #.......#.....#.#...#...............#...#  
-  #############.#.#.###.###################  
-               A O F   N                     
-               A A D   M                    """
-
-def intcode(data, prog_in):
-    inp = defaultdict(int)
-    inp.update({i: v for i,v in enumerate([int(i) for i in data.split(",")])})
-    pointer = 0
-    rel_base = 0
-    op = inp[pointer]
-    opcode = op % 100
-    modes = [(op % 10**(x+1))//(10**x) for x in reversed(range(2, 5))]
-    
-    def args(num, pos = False):
-        if modes[3-num] == 0:
-            return inp[inp[pointer+num]] if not pos else inp[pointer+num]
-        elif modes[3-num] == 2:
-            return inp[rel_base + inp[pointer+num]] if not pos else rel_base + inp[pointer+num]
-        else:
-            return inp[pointer+num]
-
-    while opcode != 99:
-        if opcode == 1:
-            inp[args(3, True)] = args(1) + args(2)
-            pointer += 4
-        elif opcode == 2:
-            inp[args(3, True)] = args(1) * args(2)
-            pointer += 4
-        elif opcode == 3:
-            inp[args(1, True)] = prog_in.pop()
-            pointer += 2
-        elif opcode == 4:
-            yield args(1)
-            pointer += 2
-        elif opcode == 5:
-            pointer = pointer + 3 if args(1) == 0 else args(2)
-        elif opcode == 6:
-            pointer = pointer + 3 if args(1) != 0 else args(2)
-        elif opcode == 7:
-            inp[args(3, True)] = 1 if args(1) < args(2) else 0
-            pointer += 4
-        elif opcode == 8:
-            inp[args(3, True)] = 1 if args(1) == args(2) else 0
-            pointer += 4
-        elif opcode == 9:
-            rel_base += args(1)
-            pointer += 2
-
-        op = inp[pointer]
-        opcode = op % 100
-        modes = [(op % 10**(x+1))//(10**x) for x in reversed(range(2, 5))]
-
-    yield "END"
-
-def draw(grid):
-    minx = int(min(z.real for z in grid.keys()))
-    maxx = int(max(z.real for z in grid.keys()))
-    miny = int(min(z.imag for z in grid.keys()))
-    maxy = int(max(z.imag for z in grid.keys()))
-    for y in range(miny, maxy+1):
-        for x in range(minx, maxx+1):
-            val = grid[complex(x, y)]
-            print(val if val != 0 else " ", end="")
-        print("\n", end="")
-    print("\n\n", end="")
-
 def solve(inp=input_data):
     grid = defaultdict(int)
     grid.update({complex(x, y): v for y, row in enumerate(inp.splitlines()) for x,v in enumerate(row)})
@@ -149,28 +49,29 @@ def solve(inp=input_data):
     tiles = [k for k,i in grid.items() if i == "."]
 
     Node = namedtuple("Node", ["pos", "last", "level"])
-    nodes = [Node(start, None, 0)]
+    nodes = [Node(start, [start + 1j**i for i in range(4) if grid[start + 1j**i] in string.ascii_uppercase][0], 0)]
     counter = 0
-    visited = set()
     while True:
         old_nodes = nodes.copy()
         nodes.clear()
-        print(len(visited))
+        print(counter)
         for n in old_nodes:
-            visited.add(n.pos)
             if n.pos == end and n.level == 0:
                 return shortest_path_length(G, start, end), counter
+            if n.level > 25:
+                continue
             adjacent = [n.pos + 1j**i for i in range(4)]
+            adjacent.remove(n.last)
             for i in adjacent:
-                if i in tiles and i != n.last:
+                if i in tiles:
                     nodes.append(Node(i, n.pos, n.level))
                 elif i in portal_cs_outer and n.level != 0:
                     nodes.append(Node(portals_inner[portals[i]][1], portals_inner[portals[i]][0], n.level-1))
                 elif i in portal_cs_inner:
-                    nodes.append(Node(portals_outer[portals[i]][1], portals_inner[portals[i]][0], n.level+1))
+                    nodes.append(Node(portals_outer[portals[i]][1], portals_outer[portals[i]][0], n.level+1))
 
         counter += 1
 
-a, b = solve(test1)
+a, b = solve()
 print(f"Part 1: {a}") 
 print(f"Part 2: {b}") 
