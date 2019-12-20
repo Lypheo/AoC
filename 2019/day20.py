dc0baf8f70ff96252b0757d24dfe67239723364d
Lypheo <saifullah124@hotmail.de>
2019-12-20 11:46:49 +0100

    day 20 attempt at part 2

--- 2019/day20.py
+++ 2019/day20.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from aocd.models import Puzzle
 from aocd import submit
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 import itertools, string
 import networkx as nx
+from networkx.algorithms.shortest_paths.generic import shortest_path_length
 
 day = datetime.today().day
 puzzle = Puzzle(year=2019, day=day)
@@ -112,10 +113,13 @@ def draw(grid):
 def solve(inp=input_data):
     grid = defaultdict(int)
     grid.update({complex(x, y): v for y, row in enumerate(inp.splitlines()) for x,v in enumerate(row)})
+    w,h = len(inp.splitlines()[0]), len(inp.splitlines())
     
     G = nx.Graph()
     G.add_nodes_from(k for k,v in grid.items() if v ==".")
 
+    portals_outer = defaultdict(int)
+    portals_inner = defaultdict(int)
     portals = defaultdict(int)
     for n,v in grid.items():
         if v != ".":
@@ -130,19 +134,43 @@ def solve(inp=input_data):
                     if portal == tuple(sorted("AA")):
                         start = n
                     elif portal == tuple(sorted("ZZ")):
-                        print(i, n)
                         end = n
                     else:
-                        portals[(i,n)] = portal
+                        portals[i] = portal
+                        if i.real < 2 or i.real > w - 3 or i.imag < 2 or i.imag > h -3:
+                            portals_outer[portal] = (i,n)
+                        else:
+                            portals_inner[portal] = (i,n)
 
-    for c,p in portals.items():
-        for c2,p2 in portals.items():
-            if p == p2 and c != c2:
-                G.add_edge(c[1], c2[1])
+    G.add_edges_from((c[1], portals_outer[p][1]) for p,c in portals_inner.items())
 
-    dkstra = nx.algorithms.shortest_paths.generic.shortest_path_length
-    return dkstra(G, start, end), None
+    portal_cs_inner = list(zip(*portals_inner.values()))[0]
+    portal_cs_outer = list(zip(*portals_outer.values()))[0]
+    tiles = [k for k,i in grid.items() if i == "."]
 
-a, b = solve()
+    Node = namedtuple("Node", ["pos", "last", "level"])
+    nodes = [Node(start, None, 0)]
+    counter = 0
+    visited = set()
+    while True:
+        old_nodes = nodes.copy()
+        nodes.clear()
+        print(len(visited))
+        for n in old_nodes:
+            visited.add(n.pos)
+            if n.pos == end and n.level == 0:
+                return shortest_path_length(G, start, end), counter
+            adjacent = [n.pos + 1j**i for i in range(4)]
+            for i in adjacent:
+                if i in tiles and i != n.last:
+                    nodes.append(Node(i, n.pos, n.level))
+                elif i in portal_cs_outer and n.level != 0:
+                    nodes.append(Node(portals_inner[portals[i]][1], portals_inner[portals[i]][0], n.level-1))
+                elif i in portal_cs_inner:
+                    nodes.append(Node(portals_outer[portals[i]][1], portals_inner[portals[i]][0], n.level+1))
+
+        counter += 1
+
+a, b = solve(test1)
 print(f"Part 1: {a}") 
 print(f"Part 2: {b}") 
