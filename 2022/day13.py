2b421ab29d5782e050bf31923e76f12b0dca5974
Lypheo <saifullah124@hotmail.de>
2022-12-14 07:21:38 +0100

    14

--- 2022/day13.py
+++ 2022/day13.py
@@ -29,9 +29,9 @@ def solve_a(inp=input_data):
             return a - b
         elif isinstance(a, list) and isinstance(b, list):
             for p, q in zip_longest(a,b, fillvalue=None):
-                if p != None and q == None:
+                if q == None:
                     return 1
-                elif p == None and q != None:
+                elif p == None:
                     return -1
                 cmp = right_order(p, q)
                 if cmp != 0:
@@ -44,7 +44,6 @@ def solve_a(inp=input_data):
 
     for i, pair in enumerate(pairs):
         p1, p2 = map(eval, pair.split("\n"))
-        print(p1, p2, right_order(p1, p2), cnt)
         cnt += i + 1 if right_order(p1, p2) < 0 else 0
     return cnt
 
@@ -106,15 +105,15 @@ tests = {
 [1,[2,[3,[4,[5,6,0]]]],8,9]""" : [13, 140]
 }
 
-# test(tests, solve_a, 0)
+test(tests, solve_a, 0)
 # a = solve_a()
 # print(f"Part 1: {a}\n")
 # submit(int(a) if isinstance(a, float) else a, part="a", day=day, year=2022)
 #
-# test(tests, solve_b, 1)
-b = solve_b()
-print(f"Part 2: {b}")
-submit(int(b) if isinstance(b, float) else b, part="b", day=day, year=2022)
+test(tests, solve_b, 1)
+# b = solve_b()
+# print(f"Part 2: {b}")
+# submit(int(b) if isinstance(b, float) else b, part="b", day=day, year=2022)
 #
 #
 # import time
