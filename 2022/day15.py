69b31a1cfe1041704ffee0aefa787fdfe1900e9c
Lypheo <saifullah124@hotmail.de>
2022-12-15 15:41:04 +0100

    clean up

--- 2022/day15.py
+++ 2022/day15.py
@@ -5,46 +5,30 @@ from aocd import submit
 from collections import defaultdict as dd
 from itertools import *
 from pprint import pprint
+import sys
+sys.path.append("..")
+from aocl import *
 
 day = 15
 puzzle = Puzzle(year=2022, day=day)
 input_data = puzzle.input_data
-"Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
-def test(tests, solution, part):
-    c = 1
-    for i,o in tests.items():
-        if o[part] and (ao := solution(i)) != o[part]:
-            print(f"Testcase {c} failed:\n    Expected output: {o}\n    Actual output: {ao}\n")
-            return False
-        c += 1
-    print(f"Tests successful!")
-    return True
 
-mh_dist = lambda a,b: int(abs(a.real - b.real) + abs(a.imag - b.imag))
+def line_intersection(line1, line2):
+    xdiff = complex(line1[0].real - line1[1].real, line2[0].real - line2[1].real)
+    ydiff = complex(line1[0].imag - line1[1].imag, line2[0].imag - line2[1].imag)
 
-def sr(a, b=None, step=1, inc=False): # signed range
-    if b == None:
-        start, end = 0, a
-    else:
-        start, end = a, b
-    if inc:
-        end += 1 if end >= start else -1
-    return range(start, end, step if end >= start else -step)
+    def det(a, b):
+        return a.real * b.imag - a.imag * b.real
 
-def ip(st, e): # interpolate complex numbers
-    diff = e - st
-    rng = []
-    if diff.real == 0:
-        rng = [complex(st.real, st.imag + y) for y in sr(int(diff.imag), inc=True)]
-    elif diff.imag == 0:
-        rng = [complex(st.real + x, st.imag) for x in sr(int(diff.real), inc=True)]
-    elif abs(diff.imag) > abs(diff.real):
-        for x in sr(int(diff.real), inc=True):
-            rng.append(complex(st.real + x, st.imag + x * diff.imag / diff.real))
-    else:
-        for y in sr(int(diff.imag), inc=True):
-            rng.append(complex(st.real + y * diff.real / diff.imag, st.imag + y))
-    return rng
+    div = det(xdiff, ydiff)
+    if div == 0:
+        # lines do not intersect
+        return False
+
+    d = complex(det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return complex(x, y)
 
 def solve_a(inp=input_data, test = False):
     yrow = (2000000 if not test else 10)
@@ -97,19 +81,26 @@ def solve_b(inp=input_data, test = False):
             pairs.append((s1, s2))
 
     for (s1, s2), (s3, s4) in combinations(pairs, 2):
+        #        ##S##
+        #         ###d#
+        #          #d#S#
+        #             #
+        # basic idea: find the diagonals ("d" in the drawing above) between 2 adjacent sensor circles
+        # (adjacent = seperated by a line of width 1)
+        # compute the intersection of the diagonals for all pairs of pairs of adjacent sensor border circles
+        # if it exists, it must contain a beacon, but it can’t be a known beacon because then its sensor would have to be on top of it
         s1, s2 = sorted([s1,s2], key= lambda s: s.real)
         s3, s4 = sorted([s3,s4], key= lambda s: s.real)
-        borders = []
+        diags = []
         for s, os in zip([s1, s3], [s2, s4]):
             d = ds[s] + 1
             od = ds[os] + 1
             e = complex(s.real + d, s.imag)
             st = complex(os.real - od, os.imag)
-            borders.append(set(ip(st, e)))
-        beacon = set.intersection(*borders)
-        if len(beacon) == 1:
-            b = beacon.pop()
-            # if not any(mh_dist(b, s) <= ds[s] for s in ds.keys()):
+            diags.append([st, e])
+
+        b = line_intersection(*diags)
+        if b != False:
             return b.real * 4000000 + b.imag
 
 tests = {
---
+++ aocl.py
@@ -0,0 +1,35 @@
+def test(tests, solution, part):
+    c = 1
+    for i,o in tests.items():
+        if o[part] and (ao := solution(i)) != o[part]:
+            print(f"Testcase {c} failed:\n    Expected output: {o}\n    Actual output: {ao}\n")
+            return False
+        c += 1
+    print(f"Tests successful!")
+    return True
+
+mh_dist = lambda a,b: int(abs(a.real - b.real) + abs(a.imag - b.imag))
+
+def sr(a, b=None, step=1, inc=False): # signed range
+    if b == None:
+        start, end = 0, a
+    else:
+        start, end = a, b
+    if inc:
+        end += 1 if end >= start else -1
+    return range(start, end, step if end >= start else -step)
+
+def ip(st, e): # interpolate complex numbers
+    diff = e - st
+    rng = []
+    if diff.real == 0:
+        rng = [complex(st.real, st.imag + y) for y in sr(int(diff.imag), inc=True)]
+    elif diff.imag == 0:
+        rng = [complex(st.real + x, st.imag) for x in sr(int(diff.real), inc=True)]
+    elif abs(diff.imag) > abs(diff.real):
+        for x in sr(int(diff.real), inc=True):
+            rng.append(complex(st.real + x, st.imag + x * diff.imag / diff.real))
+    else:
+        for y in sr(int(diff.imag), inc=True):
+            rng.append(complex(st.real + y * diff.real / diff.imag, st.imag + y))
+    return rng
