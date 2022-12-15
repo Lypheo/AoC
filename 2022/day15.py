4ca4bcb68f2107ea6a39979ccacd9b3c6595dbd1
Lypheo <saifullah124@hotmail.de>
2022-12-15 15:09:56 +0100

    optimize further

--- 2022/day15.py
+++ 2022/day15.py
@@ -21,6 +21,31 @@ def test(tests, solution, part):
     return True
 
 mh_dist = lambda a,b: int(abs(a.real - b.real) + abs(a.imag - b.imag))
+
+def sr(a, b=None, step=1, inc=False): # signed range
+    if b == None:
+        start, end = 0, a
+    else:
+        start, end = a, b
+    if inc:
+        end += 1 if end >= start else -1
+    return range(start, end, step if end >= start else -step)
+
+def ip(st, e): # interpolate complex numbers
+    diff = e - st
+    rng = []
+    if diff.real == 0:
+        rng = [complex(st.real, st.imag + y) for y in sr(int(diff.imag), inc=True)]
+    elif diff.imag == 0:
+        rng = [complex(st.real + x, st.imag) for x in sr(int(diff.real), inc=True)]
+    elif abs(diff.imag) > abs(diff.real):
+        for x in sr(int(diff.real), inc=True):
+            rng.append(complex(st.real + x, st.imag + x * diff.imag / diff.real))
+    else:
+        for y in sr(int(diff.imag), inc=True):
+            rng.append(complex(st.real + y * diff.real / diff.imag, st.imag + y))
+    return rng
+
 def solve_a(inp=input_data, test = False):
     yrow = (2000000 if not test else 10)
     inp = inp.splitlines()
@@ -78,16 +103,14 @@ def solve_b(inp=input_data, test = False):
         for s, os in zip([s1, s3], [s2, s4]):
             d = ds[s] + 1
             od = ds[os] + 1
-            border = set()
-            sign = -1 if s.imag > os.imag else 1
-            border.update(complex(s.real + x, s.imag + (d-x) * sign) for x in range(0, d+1))
-            border.update(complex(os.real - x, os.imag + (od - x) * -sign) for x in range(0, od+1))
-            borders.append(border)
+            e = complex(s.real + d, s.imag)
+            st = complex(os.real - od, os.imag)
+            borders.append(set(ip(st, e)))
         beacon = set.intersection(*borders)
         if len(beacon) == 1:
             b = beacon.pop()
-            if not any(mh_dist(b, s) <= ds[s] for s in ds.keys()):
-                return b.real * 4000000 + b.imag
+            # if not any(mh_dist(b, s) <= ds[s] for s in ds.keys()):
+            return b.real * 4000000 + b.imag
 
 tests = {
     """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -119,8 +142,8 @@ print(f"Part 2: {b}")
 
 import time
 t1 = time.time_ns()
-for i in range(times := 100):
+for i in range(times := 20):
     # solve_b()
-    solve_a()
+    solve_b()
 t2 = time.time_ns()
 print(f"Time: {(t2-t1)/(1000000*times)} ms")
