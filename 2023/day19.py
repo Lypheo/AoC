e7fbd9e575e62059cc7cc7ae40b4a63f5b3fb352
Lypheo <saifullah124@hotmail.de>
2023-12-19 07:17:10 +0100

    d19p2

--- 2023/day19.py
+++ 2023/day19.py
@@ -35,12 +35,7 @@ res = 0
 
 rules_, parts_ = inp.split("\n\n")
 rules_,parts_ = rules_.split(), parts_.split()
-parts = []
-for p in parts_:
-    p = p.strip("{}")
-    parts.append(dict((m.split("=")[0], int(m.split("=")[1])) for m in p.split(",")))
 workflows = dd(list)
-d = {'x': 1679, 'm': 44, 'a': 3000, 's': 496}
 
 for r in rules_:
     name, rr = r.split("{")
@@ -48,25 +43,42 @@ for r in rules_:
     for rule in rr:
         if ":" in rule:
             cond_, dest = rule.split(":")
-            cond = (cond_[0], cond_[1:])
+            cond = (cond_[0], cond_[1], int(cond_[2:]))
             workflows[name].append([cond, dest])
         else:
             cond = None
             dest = rule
             workflows[name].append([cond, dest])
-for item in parts:
-    wf = "in"
-    while wf not in ("A", "R"):
-        print(item, wf)
-        for cond, dest in workflows[wf]:
-            y = cond is None or (eval(f"{item[cond[0]]}{cond[1]}"))
-            if y:
-                wf = dest
-                break
-        else: assert False
-    if wf == "A":
-        res += sum(item.values())
 
+def f(wf, x):
+    global res
+    if wf == "A":
+        res += prod(r[1] - r[0] + 1 for r in x)
+    elif wf == "R":
+        return
+
+    for cond, dest in workflows[wf]:
+        if cond is None:
+            f(dest, x)
+            return
+        m, c, num = cond
+        ind = dict(x=0, m=1, a=2, s=3)[m]
+        r = x[ind]
+        if c == "<":
+            start, end = num, num - 1
+            r1 = [r[0], end]
+            r2 = [start, r[1]]
+        else:
+            start, end = num + 1, num
+            r1 = [start, r[1]]
+            r2 = [r[0], end]
+        newx = x.copy()
+        newx[ind] = r1
+        x[ind] = r2
+        f(dest, newx)
+
+q = [[1, 4000], [1, 4000], [1, 4000], [1, 4000]]
+f("in", q)
 print(f"Solution: {res}\n")
 # submit(res)
 
