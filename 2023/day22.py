c3ce23e184c05dd95ced9f49aafc2c70fe3fda2c
Lypheo <saifullah124@hotmail.de>
2023-12-22 10:26:40 +0100

    cleanup

--- 2023/day22.py
+++ 2023/day22.py
@@ -21,16 +21,15 @@ inp = """
 1,1,8~1,1,9
 """.strip()
 inp = puzzle.input_data
-
+import time
+t1 = time.time_ns()
 inp = lines(inp)
 res = 0
 bricks = []
-maxz = 0
 for line in inp:
     s, e = line.split("~")
     s = tuple(int(x) for x in s.split(","))
     e = tuple(int(x) for x in e.split(","))
-    maxz = max([maxz, s[2], e[2]])
     bricks.append(((complex(*s[:2]), s[2]), (complex(*e[:2]), e[2])))
 
 bricks.sort(key=lambda p: min(p[0][1], p[1][1]))
@@ -38,52 +37,43 @@ fallen_bricks = []
 def zs_below(s, e, fallen):
     r = set(ipl(s[0], e[0]))
     curz = max(s[1], e[1])
-    out = {}
+    out = 0
     for ss, ee in fallen:
         z = max(ss[1], ee[1])
         if z >= curz:
             continue
-        for p in set(ipl(ss[0], ee[0])) & r:
-            out[p] = max(out.get(p, 0), z)
-    assert all(p in r for p in out.keys())
+        if set(ipl(ss[0], ee[0])) & r:
+            out = max(out, z)
     return out
 
 def drop(brick, fallen):
     s, e = brick
     below = zs_below(s, e, fallen)
-    if not below:
-        z = 1
-    else:
-        z = max(below.values()) + 1
+    z = 1 + below
     zd = min(s[1] - z, e[1] - z)
     return (s[0], s[1] - zd), (e[0], e[1] - zd)
 
 while bricks:
     fallen_bricks.append(drop(bricks.pop(0), fallen_bricks))
 
-
-foundation_bricks = dd(list) # top -> bottom
+rests_on = dd(list) # top -> bottom
 supports = dd(list) # bottom -> top
-i = 0
 for s, e in fallen_bricks:
-    print(i:= i+1, len(fallen_bricks))
     possible_contacts = set((p, zz-1) for p in ip(s[0], e[0]) for zz in sri(s[1], e[1]))
     if s[1] != e[1]:
         possible_contacts = {min(possible_contacts, key=lambda x: x[1])}
 
-    contacts = set()
     for ss, ee in fallen_bricks:
         cubes = set((p, zz) for p in ip(ss[0], ee[0]) for zz in sri(ss[1], ee[1]))
         overlap = cubes & possible_contacts
         if overlap:
-            contacts.update(overlap)
-            foundation_bricks[(s, e)].append((ss, ee))
+            rests_on[(s, e)].append((ss, ee))
             supports[(ss, ee)].append((s, e))
 
 part1 = len(fallen_bricks)
 for s, e in fallen_bricks:
     for ss, ee in fallen_bricks:
-        foundation = foundation_bricks[(ss, ee)]
+        foundation = rests_on[(ss, ee)]
         if len(foundation) == 1 and foundation[0] == (s, e):
             part1 -= 1
             break
@@ -96,23 +86,19 @@ for bottom in supports.copy():
     while tops:
         new_drops = set()
         for top in tops:
-            foundation = foundation_bricks[top]
+            foundation = rests_on[top]
             if all(supporter in dropped for supporter in foundation):
                 new_drops.add(top)
 
-        tops = set()
-        for top in new_drops:
-            tops |= set(supports[top])
-
+        tops = set().union(*(set(supports[top]) for top in new_drops))
         dropped.update(new_drops)
+
     res += len(dropped) - 1
 
-print(f"Solution: {res}\n")
+print(f"Part 2: {res}\n")
 # submit(res)
 
-# import time
-# t1 = time.time_ns()
 # for i in range(times := 1000):
 #     solve_b()
-# t2 = time.time_ns()
-# print(f"Time: {(t2-t1)/(1000000*times)} ms")
+t2 = time.time_ns()
+print(f"Time: {(t2-t1)/1000000} ms")
