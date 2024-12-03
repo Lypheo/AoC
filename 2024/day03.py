f9928f0fd7dc244b549d191a3604f8427e7ee37c
Lypheo <saifullah124@hotmail.de>
2024-12-03 20:57:15 +0100

    day 3 gold

--- 2024/day03.py
+++ 2024/day03.py
@@ -1,24 +1,14 @@
-import time
-import functools, itertools, collections, re
 from aocd.models import Puzzle
-from aocd import submit
-from collections import defaultdict as dd
-from itertools import *
-from pprint import pprint
 from math import prod
-import sys
+import sys, re
 sys.path.append("..")
 from aocl import *
 from functional import seq
-from fn import _ as l
 
-day = 3
-puzzle = Puzzle(year=2024, day=day)
+puzzle = Puzzle(year=2024, day=3)
 inp = puzzle.input_data
-
 def f(chunk):
     return seq(re.findall(r"mul\(\d+,\d+\)", chunk)).map(ints).map(prod).sum()
 
 res = seq(re.split(r"do\(\)", inp)).map(lambda x: re.split(r"don't\(\)", x)[0]).map(f).sum()
-
 print(f"Solution: {res}\n")
