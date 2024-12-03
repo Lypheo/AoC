f92dde938ae899d602edf770d3e2b119388f9e32
Lypheo <saifullah124@hotmail.de>
2024-12-03 06:26:13 +0100

    day 03

--- 2024/day03.py
+++ 2024/day03.py
@@ -12,31 +12,25 @@ from aocl import *
 from functional import seq
 from fn import _ as l
 
-day = 2
+day = 3
 puzzle = Puzzle(year=2024, day=day)
+inp = """
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+""".strip()
 inp = puzzle.input_data
 
-inp = lines(inp)
-res = 0
-def check_safe(nums):
-    safe = True
-    inc = nums[1] > nums[0]
-    for a, b in pairwise(nums):
-        if inc:
-            safe = safe and b > a
-        else:
-            safe = safe and b < a
-        safe = safe and 1 <= abs(b - a) <= 3
-    return safe
+def f(chunk):
+    return sum(prod(ints(mul)) for mul in re.findall(r"mul\(\d+,\d+\)", chunk))
 
-for line in inp:
-    nums = ints(line)
-    safe = check_safe(nums)
-    for i in range(len(nums)):
-        if safe:
-            break
-        safe = check_safe(nums[:i] + nums[i+1:])
-    res += int(safe)
+res = 0
+a = inp
+while True:
+    s = re.split(r"don't\(\)", a, maxsplit=1)
+    res += f(s[0])
+    if not(len(s) > 1 and "do()" in s[1]):
+        break
+    a = re.split(r"do\(\)", s[1], maxsplit=1)[1]
 
 print(f"Solution: {res}\n")
 # submit(res)
