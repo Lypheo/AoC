c4ba80e6a22a6dabf2171d8be7aea05cd7771e39
Lypheo <saifullah124@hotmail.de>
2024-12-07 06:37:05 +0100

    day 07 unwashed ass

---
+++ 2024/day07.py
@@ -0,0 +1,49 @@
+import time
+import functools, itertools, collections, re
+from aocd.models import Puzzle
+from aocd import submit
+from collections import defaultdict as dd
+from itertools import *
+from pprint import pprint
+from math import prod
+import sys
+sys.path.append("..")
+from aocl import *
+from functional import seq
+from fn import _ as l
+
+day = 7
+puzzle = Puzzle(year=2024, day=day)
+inp = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+""".strip()
+inp = puzzle.input_data
+
+inp = lines(inp)
+sol = 0
+for line in inp:
+    res, *nums = ints(line)
+    if int(seq(nums).make_string("")) < res:
+        continue
+    for ops in product(["*", "+", ""], repeat=len(nums)-1):
+        ops = list(ops)
+        s = nums[0]
+        for b in nums[1:]:
+            if s > res:
+                break
+            s = eval(f"{s}{ops.pop()}{b}")
+        if s == res:
+            sol += res
+            break
+
+
+print(f"Solution: {sol}\n")
+# submit(sol)
