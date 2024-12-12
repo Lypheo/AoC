739ca255f782ab678b4da1eb6e131e59526b3bf6
Lypheo <saifullah124@hotmail.de>
2024-12-12 08:20:59 +0100

    day 12

--- 2024/day12.py
+++ 2024/day12.py
@@ -44,7 +44,6 @@ for p in grid:
                 seen.add(pp)
                 q.append(pp)
                 region.add(pp)
-        # print(p, q)
     regions.append(region)
 
 def cost(region):
@@ -65,52 +64,11 @@ def cost2(region):
         diags = {x for x in nbd(p) if x in nregion} - nbs
         if len(nbs) == 2 and len(diags) in [1,2] or len(nbs) == 4 and len(diags) == 3:
             corners += 1
-    num_sides = corners
-    # perimeter = set()
-    # for p in region:
-    #     for pp in nb(p):
-    #         if pp not in region:
-    #             perimeter.add(pp)
-    #
-    # sides_seen = set()
-    # sides = []
-    # for p in perimeter:
-    #     if p in sides_seen:
-    #         continue
-    #     sides_seen.add(p)
-    #     b = False
-    #     for dir in [1, 1j]:
-    #         side = {p}
-    #         for sign in [1, -1]:
-    #             pp = p
-    #             dir *= sign
-    #             while pp + dir in perimeter:
-    #                 pp += dir
-    #                 side.add(pp)
-    #                 sides_seen.add(pp)
-    #         if len(side) > 1:
-    #             sides.append(side)
-    #             b = True
-    #     if not b:
-    #         sides.append({p})
-    #
-    # num_sides = len(sides)
-    # for side in sides:
-    #     if bar := seq(side).map(lambda x: seq(nb(x)).filter(lambda y: y in region).len()).find(l >= 3):
-    #         num_sides +=  bar - 1
-    #         continue
-    #     if len(side) > 1:
-    #         continue
-    #     p = side.pop()
-    #     foo = seq(perimeter).filter(lambda x: x in nbd(p)).map(p - l).to_set()
-    #     if len(foo) == 2 and foo.pop() == - foo.pop():
-    #         num_sides += 1
-    print(grid.get(region.pop()), area, num_sides)
-    return area * num_sides
+    return area * corners
 
-res = sum(cost2(region) for region in regions)
-print(f"Solution: {res}\n")
+p1 = sum(cost(region) for region in regions)
+p2 = sum(cost2(region) for region in regions)
+print(f"Solution: {p1, p2}\n")
 copy(res)
-# submit(res)
 
 print("----%.2f s----"%(time.time()-st))
