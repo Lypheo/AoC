4bab640ce01bcc56e4559c46058908c73e1a21f1
Lypheo <saifullah124@hotmail.de>
2024-12-12 08:51:33 +0100

    day 12 c

--- 2024/day12.py
+++ 2024/day12.py
@@ -40,16 +40,9 @@ def f(p, v):
 regions = [f(p, grid[p]) for p in grid]
 
 def cost(region):
-    area = len(region)
-    perimeter = 0
-    for p in region:
-        for pp in nb(p):
-            if pp not in region:
-                perimeter += 1
-    return area * perimeter
+    return len(region) * sum(pp not in region for p in region for pp in nb(p))
 
 def cost2(region):
-    area = len(region)
     nregion = seq(region).map(l*2).map(lambda x: [x, x+1, x+1j, x+1+1j]).flatten().to_set()
     corners = 0
     for p in nregion:
@@ -57,7 +50,7 @@ def cost2(region):
         diags = {x for x in nbd(p) if x in nregion} - nbs
         if len(nbs) == 2 and len(diags) in [1,2] or len(nbs) == 4 and len(diags) == 3:
             corners += 1
-    return area * corners
+    return len(region) * corners
 
 p1 = sum(cost(region) for region in regions)
 p2 = sum(cost2(region) for region in regions)
