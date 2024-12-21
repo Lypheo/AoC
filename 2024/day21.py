0cc3554fb7194b1ec7cd01f8bc8eaf574410d652
Lypheo <saifullah124@hotmail.de>
2024-12-21 17:00:10 +0100

    part 2 clean

--- 2024/day21.py
+++ 2024/day21.py
@@ -29,13 +29,14 @@ numpad_pos = {'7': 0, '8': 1, '9': 2, '4': 1j, '5': (1+1j), '6': (2+1j), '1': 2j
 dirpad_pos = {'^': 1, 'A': 2, '<': 1j, 'v': (1+1j), '>': (2+1j), 'G': 0}
 dirs = {1j: "v", -1j: "^", 1: ">", -1:"<"}
 
-def pad2(code, numpad=True):
+def expand(code, numpad=True):
     pos_dict = numpad_pos if numpad else dirpad_pos
     pos = pos_dict["A"]
     gap = pos_dict["G"]
     outs = [""]
     def app(x, a=outs):
         a[:] = [o + x for o in a]
+
     def walk(dir):
         out = ""
         unit = dir/abs(dir) if dir else None
@@ -61,77 +62,19 @@ def pad2(code, numpad=True):
     return outs
 
 @functools.cache
-def pad(code, numpad=False):
-    pos_dict = numpad_pos if numpad else dirpad_pos
-    pos = pos_dict["A"]
-    gap = pos_dict["G"]
-    out = ""
-    def walk(dir):
-        nonlocal out
-        unit = dir/abs(dir) if dir else None
-        while dir:
-            out += dirs[unit]
-            dir -= unit
-
-    for num in code:
-        goal = pos_dict[num]
-        d = goal - pos
-        if pos.real == gap.real and goal.imag == gap.imag:
-            walk(d.real); walk(d-d.real)
-        elif pos.imag == gap.imag and goal.real == gap.real:
-            walk(d-d.real); walk(d.real)
-        else:
-            walk(d-d.real)
-            walk(d.real)
-        out += "A"
-        pos = goal
-    return out
-
-# DP = {}
-# def f(chunk, depth):
-#     if (chunk, depth) in DP:
-#         return DP[(chunk, depth)]
-#     else:
-#         chunks = re.findall(r".*A", chunk)
-#         c = chunks.pop(0)
-#         while chunks:
-#             if (c, depth) in DP:
-#                 ans = DP[(c, depth)] + f(sum(chunks, ""), depth)
-#                 break
-#             c += chunks.pop(0)
-#         else:
-#             if depth == 0:
-#                 ans= len(chunk)
-#             else:
-#                 tf = pad(chunk)
-#                 # chunks = re.findall(r".*A", tf)
-#                 # ans = sum(f(c, depth-1) for c in chunks)
-#                 ans = f(tf, depth-1)
-#     DP[(chunk, depth)] = ans
-#     return ans
-
-# 189357384273226 too high
-# 304173986714766 too high
-@functools.cache
-def f(chunk, depth):
+def f(chunk, depth, part2=False):
     if depth == 0:
         return len(chunk)
-    ways = pad2(chunk, numpad=False)
-    # chunks = re.findall(r".*?A", way)
-    return min(sum(f(c, depth-1) for c in re.findall(r".*?A", way)) for way in ways)
+    ways = expand(chunk, numpad = depth==(26 if part2 else 3))
+    return min(sum(f(c, depth-1, part2) for c in re.findall(r".*?A", way)) for way in ways)
 
 inp = lines(inp)
-res = 0
+p1, p2 = 0, 0
 for line in inp:
-    paths = pad2(line)
-    print(line, paths)
-    minval = 1e99
-    for path in paths:
-        minval = min(minval, f(path, 25))
-    res += minval * ints(line)[0]
+    p1 += f(line, 3, False) * ints(line)[0]
+    p2 += f(line, 26, True) * ints(line)[0]
 
-
-print(f"Solution: {res}\n")
+print(f"Solution: {p1, p2}\n")
 copy(res)
 # submit(res)
 
