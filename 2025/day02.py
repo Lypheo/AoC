25ad9c4dd01c7c94c9de39b9667e533509ee6cb4
Lypheo <saifullah124@hotmail.de>
2025-12-02 07:11:37 +0100

    2025 day 2 p2

--- 2025/day02.py
+++ 2025/day02.py
@@ -18,7 +18,9 @@ st=time.time()
 
 puzzle = Puzzle(year=2025, day=2)
 inp = """
-
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
 """.strip()
 inp = puzzle.input_data
 
@@ -27,10 +29,14 @@ res = 0
 for start, end in ranges:
     for id_ in range(start, end +1):
         sid = str(id_)
-        if len(sid) % 2 == 1:
-            continue
-        if sid[:len(sid)//2] == sid[len(sid)//2:]:
-            res += id_
+        for i in range(2, len(sid) + 1):
+            if len(sid) % i != 0:
+                continue
+            ss = len(sid)//i
+            parts = [sid[j : j + ss] for j in range(0, len(sid), ss)]
+            if len(set(parts)) == 1:
+                res += id_
+                break
 
 print(f"Solution: {res}\n")
 copy(res)
