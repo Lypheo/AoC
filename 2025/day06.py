e6432702a089c39d633f3f9f8734e0feb724f9e6
Lypheo <saifullah124@hotmail.de>
2025-12-06 12:16:39 +0100

    2025 day 6 p2

--- 2025/day06.py
+++ 2025/day06.py
@@ -17,20 +17,31 @@ from pyperclip import copy
 st=time.time()
 
 puzzle = Puzzle(year=2025, day=6)
-inp = """
-
-""".strip()
+inp = """123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """
 inp = puzzle.input_data
 
-inp = [line.split() for line in lines(inp)]
-n = len(inp[0])
+inp = lines(inp)
+Y, X = len(inp), len(inp[0])
 res = 0
-tasks = [[line[j] for line in inp] for j in range(n)]
-for task in tasks:
-    if task[-1] == "+":
-        res += sum(int(x) for x in task[:-1])
-    else:
-        res += prod(int(x) for x in task[:-1])
+
+sep = [" "]*Y
+tasks = [[inp[y][x] for y in range(Y)] for x in range(X)] + [sep]
+
+# print(tasks)
+op = ""
+while True:
+    if sep not in tasks:
+        break
+    i = tasks.index(sep)
+    task = tasks[:i]
+    op = task[0][-1]
+    nums = [int("".join(x for x in el if x.isnumeric())) for el in task]
+    # print(nums, op)
+    res += sum(nums) if op == "+" else prod(nums)
+    tasks = tasks[i+1:]
 
 print(f"Solution: {res}\n")
 copy(res)
